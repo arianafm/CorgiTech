b2e3ccea338b34d2a10fc8cb58cf0c60c7bbc029
Ariana Fernandez <arianafm@ciencias.unam.mx>
2021-07-20 22:06:32 -0500

    Añadiendo funcionalidad para mandar correo de confirmación una vez que el usuario se registre. También se añadio el cifrado a las contraseñas.

--- PrimeraIteracion/src/controlador/usuario_controlador.py
+++ PrimeraIteracion/src/controlador/usuario_controlador.py
@@ -1,14 +1,30 @@
+import re
 import sys
 import random
 import string
+import modelo.usuario
 from flask import Flask, render_template, redirect, url_for, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import modelo.usuario
+from cryptography.fernet import Fernet
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 db = SQLAlchemy()
 ma = Marshmallow(app)
+key = Fernet.generate_key()
+fernet = Fernet(key)
+mail= Mail(app)
+correo = ''
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'corgitech2021@gmail.com'
+app.config['MAIL_PASSWORD'] = 'fbgfqabyxaijedkf'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail= Mail(app)
 
 class UsuarioEsquema(ma.Schema):
   class Meta:
@@ -18,10 +34,25 @@ usuario_esquema = UsuarioEsquema()
 usuarios_esquema = UsuarioEsquema(many=True)
 
 def generar_contrasena():
+  """ 
+  Función que se encarga de generar una cadena
+  aleatoria de longitud 10 y después la cifra.
+  """
   contrasena = ''.join(random.sample(string.ascii_lowercase, 10))
-  return contrasena
+  encContrasena = fernet.encrypt(contrasena.encode())
+  return encContrasena
+
 
 def registrar():
+  """ 
+  Función que dados los datos recabados en el
+  JSON que recibimos como petición, se encarga
+  de crear un objeto Usuario para después aña-
+  dirlo a la base de datos. Al terminar nos 
+  redirige a "correo_confirmación".
+  """
+  global correo
+
   usuario = request.json['usuario']
   correo = request.json['correo']
   telefono = request.json['telefono']
@@ -30,9 +61,23 @@ def registrar():
 
   db.session.add(usuario_nuevo)
   db.session.commit()
+  
+  return redirect(url_for('usuario_bp.correo_confirmacion'))
 
-  return "Usuario registrado."
-
+def correo_confirmacion():
+  """ 
+  Función que manda un correo de confirmación
+  de registro al usuario que se acaba de regis-
+  trar. El correo incluye nombre de usuario y
+  contraseña ya descifrada, recordemos que esta
+  última es generada por el propio sistema con
+  la ayuda de "generar_contrasena()".
+  """
+  usuario_actual = modelo.usuario.Usuario.query.filter_by(correo = correo).first()
+  msg = Message('MercaTodo: Correo de confirmación', sender = 'corgitech2021@gmail.com', recipients = [usuario_actual.correo])
+  msg.body =  'Gracias por crear su cuenta en MercaTodo.\n' + 'Su contrasena para acceder al sistema es:\n' + str(fernet.decrypt(usuario_actual.contrasena.encode()).decode())
+  mail.send(msg)
+  return "Correo enviado."
 
 
 def login():
@@ -49,3 +94,4 @@ def index():
       
   """
   return jsonify ({'msg': 'Esta es la página de publicaciones'})
+
--- PrimeraIteracion/src/rutas/usuario_bp.py
+++ PrimeraIteracion/src/rutas/usuario_bp.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
-from controlador.usuario_controlador import index, registrar
+from controlador.usuario_controlador import index, registrar, login, correo_confirmacion
 
 usuario_bp = Blueprint('usuario_bp', __name__)
 
 usuario_bp.route('/', methods=['GET'])(index)
 usuario_bp.route('/registrar', methods=['POST'])(registrar)
+usuario_bp.route('/ingresar', methods=['POST'])(login)
+usuario_bp.route('/enviar')(correo_confirmacion)
 
