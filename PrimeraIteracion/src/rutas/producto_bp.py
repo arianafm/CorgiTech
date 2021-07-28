e30540ed70a0c776c31a8b923f9c746e32082a43
UlVc <ulvc@ciencias.unam.mx>
2021-07-28 12:48:37 -0500

    Se modificó las rutas para eliminar producto

--- PrimeraIteracion/src/rutas/producto_bp.py
+++ PrimeraIteracion/src/rutas/producto_bp.py
@@ -3,7 +3,6 @@ from controlador.producto_controlador import index, crear, eliminar, actualizar
 
 producto_bp = Blueprint('producto_bp', __name__)
 
-producto_bp.route('/', methods=['GET'])(index)
+producto_bp.route('/', methods=['GET', 'POST'])(index)
 producto_bp.route('/crear', methods=['POST'])(crear)
-producto_bp.route('/eliminar', methods=['DELETE'])(eliminar)
 producto_bp.route('/actualizar', methods=['POST'])(actualizar)
